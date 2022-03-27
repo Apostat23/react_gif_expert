9f0c8de71bc713cfa70d43584be28cba62436de6
Apostat23 <88676049+Apostat23@users.noreply.github.com>
2022-03-27 01:25:53 -0600

    fix: Grid.jsx

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,6 @@ import { useFetchItems } from '../hooks/useFetchItems'
 import { Item } from './Item' 
 
 export const Grid = ({subject}) => {
-  //const [items, setItems] = useState([])
   const [errors, setErrors] = useState(null)
   const {items, loading} = useFetchItems(subject)
   
@@ -12,7 +11,6 @@ export const Grid = ({subject}) => {
     <>
       <h3> {subject} </h3>
       {loading && <p>Loading...</p>}
-      
       <div className='grid'>
         {items.length > 0 && items.map(item => (<Item key={item.id} item={item}/>))}
       </div>
